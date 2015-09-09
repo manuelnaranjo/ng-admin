b919e30d6c849716ebc048af0918cfe264f81649
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2015-09-09 12:17:49 -0300

    [boolean] Fixing display formatting for boolean fields

--- src/javascripts/ng-admin/Crud/fieldView/BooleanFieldView.js
+++ src/javascripts/ng-admin/Crud/fieldView/BooleanFieldView.js
@@ -1,6 +1,6 @@
 module.exports = {
     getReadWidget:   () => '<ma-boolean-column value="::entry.values[field.name()]"></ma-boolean-column>',
-    getLinkWidget:   () => '<a ng-click="gotoDetail()">' + module.exports.getReadWidget() + '</a>', 
+    getLinkWidget:   () => '<a ng-click="gotoDetail()">' + module.exports.getReadWidget() + '</a>',
     getFilterWidget: () => `<ma-choice-field field="::field" value="values[field.name()]" choices="[{value: 'true', label: 'true' }, { value: 'false', label: 'false' }]"></ma-choice-field>`,
-    getWriteWidget:  () => `<div class="row"><ma-choice-field class="col-sm-4 col-md-3" field="::field" value="entry.values[field.name()]"></ma-choice-field></div>`
+    getWriteWidget:  () => `<div class="row"><ma-choice-field class="col-sm-10 col-md-10" field="::field" value="entry.values[field.name()]"></ma-choice-field></div>`
 }
