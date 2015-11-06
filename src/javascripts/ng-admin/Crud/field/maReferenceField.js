260fab0ddf6c698bad11d2786d5acfe6401db925
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2015-11-05 22:06:56 -0300

    Disable ReferenceField search until ready
    Delay ReferenceField searchs until we have set the initial set of data,
    so we can prefill with the selected values

--- src/javascripts/ng-admin/Crud/field/maReferenceField.js
+++ src/javascripts/ng-admin/Crud/field/maReferenceField.js
@@ -1,4 +1,4 @@
-export default function maReferenceField(ReferenceRefresher) {
+export default function maReferenceField(ReferenceRefresher, $timeout) {
     return {
         scope: {
             'field': '&',
@@ -12,25 +12,26 @@ export default function maReferenceField(ReferenceRefresher) {
             const identifierName = field.targetEntity().identifier().name()
             scope.name = field.name();
             scope.v = field.validation();
+            scope.searchEnabled = false;
 
-            if (!field.remoteComplete()) {
-                // fetch choices from the datastore
-                let initialEntries = scope.datastore()
-                    .getEntries(field.targetEntity().uniqueId + '_choices');
-                const isCurrentValueInInitialEntries = initialEntries.filter(e => e.identifierValue === scope.value).length > 0;
-                if (scope.value && !isCurrentValueInInitialEntries) {
-                    initialEntries.push(scope.datastore()
-                        .getEntries(field.targetEntity().uniqueId + '_values')
-                        .filter(entry => entry.values[identifierName] == scope.value)
-                        .pop()
-                    );
-                }
-                const initialChoices = initialEntries.map(entry => ({
-                    value: entry.values[identifierName],
-                    label: entry.values[field.targetField().name()]
-                }));
-                scope.$broadcast('choices:update', { choices: initialChoices });
-            } else {
+            // fetch choices from the datastore
+            let initialEntries = scope.datastore()
+                .getEntries(field.targetEntity().uniqueId + '_choices');
+            const isCurrentValueInInitialEntries = initialEntries.filter(e => e.identifierValue === scope.value).length > 0;
+            if (scope.value && !isCurrentValueInInitialEntries) {
+                initialEntries.push(scope.datastore()
+                                    .getEntries(field.targetEntity().uniqueId + '_values')
+                                    .filter(entry => entry.values[identifierName] == scope.value)
+                                    .pop()
+                                   );
+            }
+            const initialChoices = initialEntries.map(entry => ({
+                value: entry.values[identifierName],
+                label: entry.values[field.targetField().name()]
+            }));
+            scope.$broadcast('choices:update', { choices: initialChoices });
+
+            if (field.remoteComplete()) {
                 // ui-select doesn't allow to prepopulate autocomplete selects, see https://github.com/angular-ui/ui-select/issues/1197
                 // let ui-select fetch the options using the ReferenceRefresher
                 scope.refresh = function refresh(search) {
@@ -39,15 +40,18 @@ export default function maReferenceField(ReferenceRefresher) {
                             scope.$broadcast('choices:update', { choices: formattedResults });
                         });
                 };
+
+                $timeout(function(){scope.searchEnabled = true;}, 0);
             }
         },
         template: `<ma-choice-field
                 field="field()"
                 datastore="datastore()"
                 refresh="refresh($search)"
+                search-enable="{{searchEnabled}}"
                 value="value">
             </ma-choice-field>`
     };
 }
 
-maReferenceField.$inject = ['ReferenceRefresher'];
+maReferenceField.$inject = ['ReferenceRefresher', '$timeout'];
